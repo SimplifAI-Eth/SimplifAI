8e7d73807bbc12a8b95a94aaae03b7a47183e64f
Noobmaster169 <mariotaning@gmail.com>
2024-11-17 08:14:47 +0800

    hope this fix

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -182,7 +182,7 @@ export default function Home() {
         // console.log("Accepted action");
         const args = processedArguments;
         executeTx(args);
-        setAcceptAction(false);
+        //setAcceptAction(false);
       }
     }, [acceptAction]);
 
